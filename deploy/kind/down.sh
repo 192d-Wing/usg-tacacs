102f42a926cc115432dcab8d4d04fb15200ba119
1456055067 <john.willman.1@us.af.mil>
2026-06-04 12:44:03 -0500

    chore: removed kind cluster deploy

--- deploy/kind/down.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# SPDX-License-Identifier: Apache-2.0
-# Delete the local kind cluster created by up.sh.
-set -euo pipefail
-CLUSTER="${1:-usg-tacacs}"
-kind delete cluster --name "$CLUSTER"
-echo "Deleted kind cluster '$CLUSTER'."
